b28a11abf18435cc6d48bc593aec1e91efc0f498
V5040 <V5040@users.noreply.github.com>
2022-12-21 10:13:28 +0300

    clear some code

--- ml_project/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .generate_dataset import split_data_train_val, read_data_pd
-
-__all__ = ["split_data_train_val", "read_data_pd"]
--- ml_project/entities/download_parameters.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from typing import List
-
-
-@dataclass()
-class DownloadParameters:
-    paths: List[str]
-    output_folder: str
-    s3_bucket: str = field(default="for-dvc")
